d26575b8e6c5ac215e32feeb549fc0f6d30c833a
Jamie van Dyke <me@fearof.fish>
2022-02-21 14:24:34 +0000

    add more things

--- flake.nix
+++ flake.nix
@@ -48,15 +48,21 @@
             };
           }
 
-          ({ pkgs, lib, ... }: {
+          ({ config, pkgs, lib, ... }: {
             home-manager.users."jamievandyke" = {
+              # home.sessionPath = [];
+              # home.sessionVariables = [];
 
-              home.packages = with pkgs; [ 
+              home.packages = with pkgs; [
+                # work tool
+                bosh-cli # cli for cloudfoundry bosh
+                safe # a vault cli
+
+                # general tools
                 asdf-vm # version manager for all the things
                 autoconf # Broadly used tool, no clue what it does
                 awscli # Amazon Web Services CLI
                 bash # /bin/bash
-                bat # cat replacement written in Rust
                 bottom # istat menus on the cli
                 buildpack # Cloud Native buildpacks
                 curl # An old classic
@@ -66,7 +72,6 @@
                 fzf # Fuzzy finder
                 gh # github cli
                 git # git maybe?
-                gnupg # gpg
                 google-cloud-sdk # Google Cloud Platform CLI
                 graphviz # dot
                 htop # Resource monitoring
@@ -103,6 +108,43 @@
 
               programs.fish = {
                 enable = true;
+                plugins = [
+                  # Need this when using Fish as a default macOS shell in order to pick
+                  # up ~/.nix-profile/bin
+                  {
+                    name = "nix-env";
+                    src = pkgs.fetchFromGitHub {
+                      owner = "lilyball";
+                      repo = "nix-env.fish";
+                      rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
+                      sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
+                    };
+                  }
+                ];
+                shellInit = ''
+                  # Set syntax highlighting colours; var names defined here:
+                  # http://fishshell.com/docs/current/index.html#variables-color
+                  set fish_color_autosuggestion brblack
+                '';
+                shellAliases = {
+                  rm = "rm -i";
+                  cp = "cp -i";
+                  mv = "mv -i";
+                  mkdir = "mkdir -p";
+                };
+                shellAbbrs = {
+                  o = "open";
+                };
+                functions = {
+                  fish_greeting = {
+                    description = "Greeting to show when starting a fish shell";
+                    body = "";
+                  };
+                  mkdcd = {
+                    description = "Make a directory tree and enter it";
+                    body = "mkdir -p $argv[1]; and cd $argv[1]";
+                  };
+                };
               };
               
               programs.direnv.enable = true;
@@ -112,10 +154,46 @@
                 userEmail = "me@fearof.fish";
                 userName = "Jamie van Dyke";
                 signing = {
-                  key = "me@fearof.fish";
+                  key = "6CBDC8C754ECEA0FDB57CE4EC89DD29350A9E797";
                   signByDefault = true;
                 };
+                delta = {
+                  enable = true;
+                  options = {
+                    navigate = true;
+                    line-numbers = true;
+                    syntax-theme = "GitHub";
+                  };
+                };
+                ignores = [ ".DS_Store" "*.swp" ];
+                extraConfig = {
+                  core = {
+                    editor = "subl -w";
+                  };
+                  color = {
+                    ui = true;
+                  };
+                  push = {
+                    default = "simple";
+                  };
+                  pull = {
+                    ff = "only";
+                  };
+                  init = {
+                    defaultBranch = "main";
+                  };
+                };
               };
+
+              programs.gpg.enable = true;
+
+              programs.bat = {
+                  enable = true;
+                  config = {
+                    theme = "GitHub";
+                    italic-text = "always";
+                  };
+                };
 
               # create some custom dot-files on your user's home.
               # home.file.".config/foo".text = "bar";
@@ -124,16 +202,21 @@
 
           # for configurable nixos modules see (note that many of them might be linux-only):
           # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/module-list.nix
-          ({ config, lib, ... }: {
-
-            # You can provide an overlay for packages not available or that fail to compile on arm.
-            # nixpkgs.overlays =
-            #  [ (self: super: { 
-            #   inherit (lib.mds.intelPkgs) cachix; }) ];
-
+          ({ config, lib, pkgs, ... }: {
             # You can enable supported services (if they work on arm and are not linux only)
             # services.lorri.enable = true;
           })
+
+          # ({ pkgs, ...}: {
+          #   # Install MacOS applications to the user environment if the targetPlatform is Darwin
+          #     home.file."Applications/home-manager".source = let
+          #     apps = pkgs.buildEnv {
+          #       name = "home-manager-applications";
+          #       paths = config.home.packages;
+          #       pathsToLink = "/Applications";
+          #     };
+          #     in mkIf pkgs.stdenv.targetPlatform.isDarwin "${apps}/Applications";
+          # })
 
         ];
       };
