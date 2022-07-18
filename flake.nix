e5444ca3e15e0d5ce72e4467be6a86b804ffc154
Jamie van Dyke <me@fearof.fish>
2022-07-18 17:44:35 +0100

    meh

--- flake.nix
+++ flake.nix
@@ -3,15 +3,12 @@
 {
   description = "Jamie's Nix Environment";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/ce6aa13369b667ac2542593170993504932eb836";
     # change tag or commit of nixpkgs for your system
-    mk-darwin-system.url = "github:vic/mk-darwin-system/main"; # change main to a tag o git revision
-    # mk-darwin-system.url = "path:/hk/mkDarwinSystem";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    # development mode
+    # change main to a tag o git revision
+    mk-darwin-system.url = "github:vic/mk-darwin-system/main";
     mk-darwin-system.inputs.nixpkgs.follows = "nixpkgs";
-    alejandra.url = "github:kamadorueda/alejandra";
-    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
     self,
